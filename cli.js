10742240fcb51632321b617d98f3cb1f665116b1
Mariana MagallanesLMML <marianamagallanespp13@gmail.com>
2023-06-15 21:56:59 -0600

    Escructura del Readme

--- cli.js
+++ cli.js
@@ -1,11 +1,11 @@
-const validateMdLinks = require('./mdLinks');
+//const validateMdLinks = require('./mdLinks');
 //const fetch = require('node-fetch');
 const {mdLinks} = require('./mdLinks');
 
 mdLinks('./markDown/routes.md', { validate: true  }).then((result) => {
-  console.log(/*'resultValidate',*/ result);
+  console.log(result);
 }).catch((error) => {
-  console.log(/*'erroValidate',*/error);
+  console.log(error);
 });
 // ruta definitiva './markDown/routes.md'
 // ruta absoluta 'C:/Users/maria/OneDrive/Escritorio/MdLinks/DEV005-md-links-lite/markDown/routes.md'
